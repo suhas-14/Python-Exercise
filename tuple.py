9b8428f273988b165cafb1e7a2d3c32e99efca47
Suhas <nanisuhas3@gmail.com>
2022-04-13 09:52:24 +0100

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,10 @@
+#adding a vlaue into a tuple
+
+myTuple = ('mango','banana','orange') 
+print(myTuple)
+print(len(myTuple))
+
+#adding value to tuple
+myTuple = myTuple + ('lichi',)
+print(myTuple)
+print(len(myTuple))
